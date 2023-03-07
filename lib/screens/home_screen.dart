dea1631c3cfaa4ab755475ccd36217f22bbe7e5b
Youssef Ahmed <41526171+youssef1331@users.noreply.github.com>
2023-03-07 19:20:34 +0200

    add last feauter

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,14 +14,14 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimatedContainer(
-        duration: const Duration(seconds: 2),
-        curve: Curves.linear,
-        color: _color,
-        child: Center(
-          child: TextButton(
-            onPressed: _generateColor,
-            child: const Text(
+      body: InkWell(
+        onTap: _generateColor,
+        child: AnimatedContainer(
+          duration: const Duration(seconds: 2),
+          curve: Curves.linear,
+          color: _color,
+          child: const Center(
+            child: Text(
               'Hey There',
               style: TextStyle(
                 color: Colors.black,
